a16b098b04d42c8578bdefd5d35436493bacd1e6
Luamix <luizalfonso611@gmail.com>
2024-03-12 14:36:03 -0500

    changes on my base model

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,6 @@ class BaseModel():
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
                 self.updated_at = datetime.now()
-                return 
 
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -17,14 +16,14 @@ class BaseModel():
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
 
         def __str__(self):
-            return f"{[self.__class__.__name__]},{(self.id)},{self.__dict__}"
+            return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
 
         def save(self):
             self.updated_at = datetime.now()
-            
+
         def to_dict(self):
             self.created_at = self.created_at.isoformat()
             self.updated_at = self.updated_at.isoformat()
-            
+
             self.__dict__.__setitem__('__class__', self.__class__.__name__)
             return self.__dict__
