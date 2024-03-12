91379ca234ba5cfdc34bdd975900e7f7957e50a9
Luamix <luizalfonso611@gmail.com>
2024-03-12 16:10:15 -0500

    move attribute id

--- models/base_model.py
+++ models/base_model.py
@@ -4,12 +4,11 @@ from datetime import datetime
 
 class BaseModel():
         def __init__(self, *args, **kwargs):
+            self.id = str(uuid.uuid4())
             if not kwargs:
-                self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
                 self.updated_at = datetime.now()
                 return
-
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
