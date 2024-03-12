70cd8098fd22385c8634fd547660012da723dcfa
Luamix <luizalfonso611@gmail.com>
2024-03-12 14:08:07 -0500

    Changes in for loop

--- models/base_model.py
+++ models/base_model.py
@@ -6,27 +6,25 @@ class BaseModel():
         def __init__(self, *args, **kwargs):
             if not kwargs:
                 self.id = str(uuid.uuid4())
-                self.create_at = datetime.now()
-                self.update_at = datetime.now()
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
 
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
-                if key == 'update_at' or key == 'create_at':
-                    self.__dict__[key] == datetime.strptime
-                    self.__dict__[key] == datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == 'updated_at' or key == 'created_at':
+                    value == datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                     continue
-                self.__dict__[key] = value
 
         def __str__(self):
             return(f"{[self.__class__.__name__]},{(self.id)},{self.__dict__}")
 
         def save(self):
-            self.update_at = datetime.now()
+            self.updated_at = datetime.now()
             
         def to_dict(self):
-            self.create_at = self.create_at.isoformat()
-            self.update_at = self.update_at.isoformat()
+            self.created_at = self.created_at.isoformat()
+            self.updated_at = self.updated_at.isoformat()
             
             self.__dict__.__setitem__('__class__', self.__class__.__name__)
-            return dict(self.__dict__)
+            return self.__dict__
