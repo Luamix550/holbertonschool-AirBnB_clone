9b2fc6575acd256042f49b0fd761ef905241d218
Luamix <luizalfonso611@gmail.com>
2024-03-12 15:57:52 -0500

    change conditional

--- models/base_model.py
+++ models/base_model.py
@@ -8,15 +8,19 @@ class BaseModel():
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
                 self.updated_at = datetime.now()
-            else:
-                for key, value in kwargs.items():
-                    if key == 'updated_at' or key == 'created_at':
-                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, value)
+                return
+
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                continue
+            self.__dict__[key] = value
 
         def __str__(self):
             return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
         def save(self):
             self.updated_at = datetime.now()
 
