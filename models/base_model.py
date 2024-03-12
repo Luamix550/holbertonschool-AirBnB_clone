54cb287c692e9855755cef697aa4caf208318fc9
Luamix <luizalfonso611@gmail.com>
2024-03-12 14:17:16 -0500

    add: function setattr

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,13 @@ class BaseModel():
                 return 
 
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
+                if key != '__class__':
+                    setattr(self, key, value)
                 if key == 'updated_at' or key == 'created_at':
-                    value == datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
 
         def __str__(self):
-            return(f"{[self.__class__.__name__]},{(self.id)},{self.__dict__}")
+            return f"{[self.__class__.__name__]},{(self.id)},{self.__dict__}"
 
         def save(self):
             self.updated_at = datetime.now()
