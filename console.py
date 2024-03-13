72c4bb541b06cb75d68e254d4a2a1754c1febe82
Luamix <luizalfonso611@gmail.com>
2024-03-13 18:15:42 -0500

    change all method

--- console.py
+++ console.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module provides the entry point of the command interpreter"""
 import cmd
-import sys
 
 
 
@@ -9,18 +8,21 @@ class HBNBCommand(cmd.Cmd):
     """This class shows the basic console"""
     prompt = "(hbnb)"
 
-    def do_quit(self, arg):
-        """This method quit the console"""
-        sys.exit()
+    def do_EOF(self, line):
+        """Handles End Of File character.
+        """
+        print()
+        return True
 
-    def do_EOF(self, arg):
-        """This method shows EOF on the console """
-        sys.exit()
+    def do_quit(self, line):
+        """Exits the program.
+        """
+        return True
 
-    def empyline(self,arg):
-        """This method is called when an empty line is entered"""
-        if arg == "" or arg == " ":
-            pass
+    def emptyline(self):
+        """Doesn't do anything on ENTER.
+        """
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
