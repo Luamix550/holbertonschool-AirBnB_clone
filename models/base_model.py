e8521dcfa03f1cc41a2d5330e017e00c638d5e0b
Luamix <luizalfonso611@gmail.com>
2024-03-13 14:44:58 -0500

    change importation

--- models/base_model.py
+++ models/base_model.py
@@ -3,32 +3,32 @@ from uuid import uuid4
 from datetime import datetime
 from models import storage
 
-class BaseModel():
-        def __init__(self, *args, **kwargs):
-            self.id = str(uuid4())
-            if not kwargs:
-                self.created_at = datetime.now()
-                self.updated_at = datetime.now()
-                return
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            # Initialize attributes from kwargs
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == 'created_at' or key == 'updated_at':
-                    self.__dict__[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                continue
-            self.__dict__[key] = value
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:
+            # Generate id if not provided
+            self.id = str(uuid4())
+            # Set created_at and updated_at timestamps
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
-        def __str__(self):
-            return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-        def save(self):
-                self.updated_at = datetime.now()
-                storage.save()
-                storage.new(self)
+    def save(self):
+        self.updated_at = datetime.now()
+        storage.save()
 
-        def to_dict(self):
-            new_dict = self.__dict__.copy()
-            new_dict['__class__'] = self.__class__.__name__
-            new_dict['created_at'] = self.created_at.isoformat()
-            new_dict['updated_at'] = self.updated_at.isoformat()
-            return new_dict
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
