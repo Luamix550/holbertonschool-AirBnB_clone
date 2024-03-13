2ee9315f719b6063ddcc15313d79ad6b991fbd18
Luamix <luizalfonso611@gmail.com>
2024-03-13 15:09:33 -0500

    add for loop in the method save and reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,26 +1,28 @@
-#!/usr/bin/python3
 import json
 import os
 
 class FileStorage:
-
-    def __init__(self):
-        self.__file_path = "file.json"
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         return self.__objects
 
     def new(self, obj):
-        key = f"{obj.__class__.__name__} {obj.id}"
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        with open(self.__file_path, "w") as write_file:
-            json.dump(self.__objects,write_file)
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(serialized_objects, file)
 
     def reload(self):
-        #try:
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, "r") as read_file:
-                data = json.load(read_file)
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                loaded_objects = json.load(file)
+                for key, obj_dict in loaded_objects.items():
+                    class_name, obj_id = key.split('.')
+                    self.__objects[key] = globals()[class_name](**obj_dict)
