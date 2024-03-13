52ae38719f293cfa39fd270317b1f23f643c315e
Luamix <luizalfonso611@gmail.com>
2024-03-13 17:40:03 -0500

    add documentation for the method and class

--- console.py
+++ console.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 import cmd
 import sys
+"""This module provides the entry point of the command interpreter"""
+
 
 class HBNBCommand(cmd.Cmd):
-    
+    """This class shows the basic console"""
     prompt = "(hbnb)"
-    
+
     def do_quit(self, arg):
+        """This method quit the console"""
         sys.exit()
-        
-    def do_EOF(self,arg):
+
+    def do_EOF(self, arg):
+        """This method shows EOF on the console """
         sys.exit()
-    
+
     do_EOF = do_quit
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
