7e50e8ecf142d03bbb878fd85753c59d6c8394c8
Luamix <luizalfonso611@gmail.com>
2024-03-14 19:13:04 -0500

    change logic of the methods show and destroy

--- console.py
+++ console.py
@@ -25,35 +25,54 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, line):
+        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id."""
         if not line:
             print("** class name missing **")
         else:
             try:
-                class_create = eval(line)()
-                class_create.save()
-                print(class_create.id)
+                new_instance = eval(line)()
+                new_instance.save()
+                print(new_instance.id)
             except NameError:
                 print("** class doesn't exist **")
 
-    def do_show(self,line):
+    def do_show(self, line):
+        """Prints the string representation of an instance based on the class name and id."""
         if not line:
             print("** class name missing **")
         else:
             try:
-                info = line.split()
-                verify_class = eval(line[0])
-                if len(info) > 2:
-                    print("** instance id missing **")
+                class_name, instance_id = line.split()
+                if class_name not in storage.all():
+                    print("** class doesn't exist **")
                 else:
-                    Class_name = info[0] + "." + info[1]
-                    if Class_name not in storage.all().keys():
+                    key = class_name + "." + instance_id
+                    if key not in storage.all()[class_name]:
                         print("** no instance found **")
                     else:
-                        for key, value in storage.all().items():
-                            if Class_name == key:
-                                print(value)
-            except NameError:
-                print("** class doesn't exist **")
+                        print(storage.all()[class_name][key])
+            except ValueError:
+                print("** instance id missing **")
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id (save the change into the JSON file)."""
+        if not line:
+            print("** class name missing **")
+        else:
+            try:
+                class_name, instance_id = line.split()
+                if class_name not in storage.all():
+                    print("** class doesn't exist **")
+                else:
+                    key = class_name + "." + instance_id
+                    if key not in storage.all()[class_name]:
+                        print("** no instance found **")
+                    else:
+                        del storage.all()[class_name][key]
+                        storage.save()
+            except ValueError:
+                if len(line.split()) == 1:
+                    print("** instance id missing **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
