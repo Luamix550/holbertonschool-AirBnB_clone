ca05281bd71885174a9e222492a6bafbb4c4892e
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-03-16 17:52:10 -0500

    update new test for to_dict method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python3
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class testBaseModel_save(unittest.TestCase):
 
-    def test_do_save(self):
+    def test_save(self):
         instance = BaseModel()
         instance.save()
         self.assertIsNotNone(instance.updated_at)
+    
+    def test_to_dict(self):
+        """Testing to_dict method creatin a directory
+        from the instance atributtes and checking if
+        it is the desired result"""
+        instance = BaseModel()
+        instance.id = '123'
+        instance.__class__ = 'Luis'
+        instance.created_at = datetime(2024, 3, 3)
+        instance.updated_at = datetime(2024, 3, 3)
+
+        actual_result = instance.to_dict()
+
+        desired_result = {
+            'id': '123'
+            '__class__': 'Luis'
+            'created_at': '2024, 3, 3'
+            'updated_at': '2024, 3, 3'
+        }
+
+        self.assertEqual(actual_result, desired_result)
 
 if __name__ == '__main__':
     unittest.main()
