d780146c7bab3308013f55f9294dcdedf566d646
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-03-16 18:25:07 -0500

    testing 'test_FileStorage'

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -5,4 +5,9 @@ import unittest
 class testFileStorage(unittest.TestCase):
     """ This class testing fileStorage """
     def test_file_path(self):
-        
+        """Method to test if the file_path method from
+        file_storage module is actually working in order
+        to succesfully create this file"""
+        instance = FileStorage()
+        desired_result = instance.save()
+        self.assertTrue(hasattr(desired_result, __file_path))
