0b96c1a172c50fc372a60a14a3030549281781ab
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-03-16 18:31:17 -0500

    updating test_file_path

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -2,12 +2,15 @@
 from models.engine.file_storage import FileStorage
 import unittest
 
-class testFileStorage(unittest.TestCase):
+class test_FileStorage(unittest.TestCase):
     """ This class testing fileStorage """
     def test_file_path(self):
         """Method to test if the file_path method from
         file_storage module is actually working in order
         to succesfully create this file"""
         instance = FileStorage()
-        desired_result = instance.save()
+        desired_file_path = "file.json"
+        self.assertFalse(hasattr(istance, '__file_path'))
+        instance.save()
         self.assertTrue(hasattr(desired_result, __file_path))
+        self.assertEqual(instance.__file_path, )
