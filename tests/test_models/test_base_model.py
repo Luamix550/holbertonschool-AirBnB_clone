8db507ffff6979d80eb23939b4a91a64216b0c41
Luamix <luizalfonso611@gmail.com>
2024-03-16 17:54:03 -0500

    change name to test Save

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,12 +3,17 @@ import unittest
 from models.base_model import BaseModel
 
 
-class testBaseModel_save(unittest.TestCase):
+class testBaseModel(unittest.TestCase):
 
-    def test_do_save(self):
+    def test_save(self):
         instance = BaseModel()
         instance.save()
         self.assertIsNotNone(instance.updated_at)
+        
+    def test_dict(self):
+    
+    
+    
 
 if __name__ == '__main__':
     unittest.main()
